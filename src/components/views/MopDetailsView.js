da69c47b21aec3af02c1c307c90aae5c10486c29
klaudialax <klaudialax@gmail.com>
2018-04-09 14:40:57 +0200

    vulnerability fix and typo fix

--- src/components/views/MopDetailsView.js
+++ src/components/views/MopDetailsView.js
@@ -57,7 +57,7 @@ export default class MopDetailsView extends Component {
     }
     else {
       return <Button
-        title='Dodaj to ulubionych'
+        title='Dodaj do ulubionych'
         onPress={() => this.addToFavourites(this.state.mop.id)}
         icon={<Icon
           name='favorite-border'
